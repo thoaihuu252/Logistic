98ce9281777a467d471758ee1969438161d5ea1a
PhamHuuThoai <thoaihuu252@gmail.com>
2024-01-03 16:24:01 +0700

    Big Update
    - Cập nhật lại các Sreen
    - Thêm mới các màn hình

--- app/src/main/java/com/example/androilogictic/CompleteSreen.kt
+++ app/src/main/java/com/example/androilogictic/CompleteSreen.kt
@@ -3,14 +3,12 @@ package com.example.androilogictic
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.LinearLayout
 
-class MainSreen : AppCompatActivity() {
+class CompleteSreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_sreen)
-
+        setContentView(R.layout.activity_complete_sreen)
         val warehouse: LinearLayout = findViewById(R.id.buttonWarehouse)
         val home: LinearLayout = findViewById(R.id.buttonHome)
         val order: LinearLayout = findViewById(R.id.buttonOrder)
@@ -26,6 +24,5 @@ class MainSreen : AppCompatActivity() {
         profile.setOnClickListener { navigateToScreen(ProfileSreen::class.java) }
         order.setOnClickListener { navigateToScreen(OrderSreen::class.java) }
         complete.setOnClickListener { navigateToScreen(CompleteSreen::class.java) }
-
     }
 }
